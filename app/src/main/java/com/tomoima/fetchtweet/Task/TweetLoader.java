ad5fe20f8391ff70df8ace7a5e48f2aeb58d5adf
tomoima525 <tomoima525@gmail.com>
2016-04-10 23:40:13 -0700

    Cosmetic change

--- app/src/main/java/com/tomoima/fetchtweet/Task/TweetLoader.java
+++ app/src/main/java/com/tomoima/fetchtweet/Task/TweetLoader.java
@@ -10,6 +10,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 import io.realm.Realm;
 import rx.Observable;
+import rx.schedulers.Schedulers;
 import rx.subjects.PublishSubject;
 import timber.log.Timber;
 
@@ -30,38 +31,7 @@ public class TweetLoader implements Runnable {
         this.threadPoolExecutor = threadPoolExecutor;
     }
 
-    public Observable<Observable<List<TweetData>>> fetchMultipleTweets(long sinceId, long maxId) {
-        CustomTwitterApiClient client = CustomTwitterApiClient.getInstance();
-        final Long tempMaxId = maxId == -1L ? null : maxId;
-        final Long tempSinceId = sinceId == -1L ? null: sinceId;
-        return Observable.create(subscriber -> {
-            try {
-                subscriber.onNext(client.getCustomStatusesService()
-                        .userTimeline(null,
-                                userName,
-                                200,
-                                tempSinceId,
-                                tempMaxId,
-                                false,
-                                false,
-                                false,
-                                true)
-                        .flatMap(Observable::from)
-                        .map(tweet -> {
-                            TweetData data = new TweetData();
-                            data.setMessage(tweet.text);
-                            data.setId(tweet.id);
-                            data.setName(tweet.user.screenName);
-                            return data;
-                        }).toList());
-                subscriber.onCompleted();
-            } catch (Exception e){
-                subscriber.onError(e);
-            }
-        });
-    }
-
-    public void fetchAllTweets_alpha(long sinceId, long maxId) {
+    public void fetchAllTweets(long sinceId, long maxId) {
         final CustomTwitterApiClient client = CustomTwitterApiClient.getInstance();
         //maxId を emit する Subject を用意して、そのツイートを取得するたびに、その onNext を叩く
         final PublishSubject<Long> maxIdSubject = PublishSubject.create();
@@ -88,6 +58,7 @@ public class TweetLoader implements Runnable {
                                 return data;
                             }).toList().map(list -> new Pair<>(mid, list));
                 })
+                .observeOn(Schedulers.from(threadPoolExecutor))
                 .subscribe(
                         p -> {
                             TweetLoader.putTweetDataList(p.second);
@@ -108,82 +79,13 @@ public class TweetLoader implements Runnable {
         maxIdSubject.onNext(maxId);
     }
 
-//    public void fetchAllTweets(long sinceId, long maxId) {
-//        Observable<Result> observable = Observable.create(subscriber -> {
-//            fetchMultipleTweets(sinceId, maxId).subscribe(new Subscriber<Observable<List<TweetData>>>() {
-//                @Override
-//                public void onCompleted() {
-//
-//                }
-//
-//                @Override
-//                public void onError(Throwable e) {
-//                    subscriber.onError(e);
-//                    subscriber.onNext(new Result(ResultCode.ERROR, e.toString(), -1, -1));
-//                    subscriber.onCompleted();
-//                }
-//
-//                @Override
-//                public void onNext(Observable<List<TweetData>> tweetDataListObservable) {
-//                    tweetDataListObservable.isEmpty().subscribe(isEmpty -> {
-//                        if (isEmpty) {
-//                            subscriber.onCompleted();
-//                        } else {
-//                            tweetDataListObservable
-//                                    .observeOn(Schedulers.from(threadPoolExecutor))
-//                                    .subscribe(TweetLoader::putTweetDataList);
-//
-//                            //最後のid取得
-//                            tweetDataListObservable.flatMap(Observable::from)
-//                                    .last().subscribe(tweetData -> {
-//                                long nextMaxId = tweetData.getId();
-//                                if (nextMaxId == maxId) {
-//                                    subscriber.onCompleted();
-//                                } else {
-//                                    subscriber.onNext(new Result(ResultCode.SUCCESS, "", sinceId, nextMaxId));
-//                                }
-//                            });
-//                        }
-//                    });
-//                }
-//            });
-//        });
-//
-//
-//
-//        observable
-//                .subscribeOn(Schedulers.newThread())
-//                .subscribe(new Subscriber<Result>() {
-//            @Override
-//            public void onCompleted() {
-//                Timber.d("¥¥ fetching completed");
-//            }
-//
-//            @Override
-//            public void onError(Throwable e) {
-//                Timber.e("¥¥ error " + e.getMessage());
-//            }
-//
-//            @Override
-//            public void onNext(Result result) {
-//                Timber.d("¥¥ recursive thread:" + Thread.currentThread().getName());
-//                if(ResultCode.ERROR.equals(result.resultCode)) {
-//                    onCompleted();
-//                    return;
-//                }
-//                fetchAllTweets(result.sinceId, result.maxId);
-//            }
-//        });
-//    }
-
     public void start(){
         threadPoolExecutor.execute(this);
     }
     @Override
     public void run() {
-        //fetchAllTweets(sinceId,maxId);
         Timber.d("¥¥ start thread: " + Thread.currentThread().getName());
-        fetchAllTweets_alpha(sinceId, maxId);
+        fetchAllTweets(sinceId, maxId);
     }
 
 
